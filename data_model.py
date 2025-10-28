0a43974a898196ea53e730e9a87c5632aa59e70f
NicoleLia <nicolia0118@gmail.com>
2025-10-28 14:06:48 +1300

    add class

---
+++ data_model.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class PaperItem(BaseModel):
+    arxiv_id: str
+    title: str
+    summary: str
+    authors: list[str]
+    pdf_url: str
+    abs_url: str
+
+
+class PaperDigest(BaseModel):
+    paper: PaperItem
+    zh_summary: str
+    main_img_bytes: bytes | None
+    main_img_cid: str | None
