990454cee2069a8bf66316f39faeccb16b06316a
NicoleLia <nicolia0118@gmail.com>
2025-10-30 23:49:24 +1300

    update multilanguage

--- data_model.py
+++ data_model.py
@@ -13,5 +13,6 @@ class PaperItem(BaseModel):
 class PaperDigest(BaseModel):
     paper: PaperItem
     zh_summary: str
+    summary_en: str
     main_img_bytes: bytes | None
     main_img_cid: str | None
